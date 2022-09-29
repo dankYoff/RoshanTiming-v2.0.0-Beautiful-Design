5ec387fec264febb30373b9c4b386b95f5e67a77
dankYoff <vsnopik@gmail.com>
2022-09-29 18:47:25 +0300

    Result + Full timing (aegis + min-man rosh)

--- app.py
+++ app.py
@@ -16,9 +16,27 @@ else:
     bb = str(str(b))
 timer = str(aa + ':' + bb)
 
+# AEGIS TIMER
+aegis_a = a + 5
+if aegis_a <= 9:
+    aegis_a = str('0' + str(aegis_a))
+else:
+    aegis_a = str(aegis_a)
+b = randint(0, 59)
+
+# MIN TIMER ROSHAN
+int_min_rosh = a + 8
+
+# MAX TIMER ROSHAN
+int_max_rosh = a + 11
+
+# Enter Aegis, min or max + Check
+c = 1
+d = str(str(aegis_a) + bb + ' ' + str(int_min_rosh) + bb + '-' + str(int_max_rosh) + bb)
+
 @app.route('/')
 def index():
-    return render_template('index.html', timer=timer)
+    return render_template('index.html', timer=timer, d=d)
 
 if __name__ == "__main__":
     app.run(debug=True)
