bc824a10e081582e1f05a78ec1cb015efd7226b7
dankYoff <vsnopik@gmail.com>
2022-09-29 05:32:30 +0300

    Fix work in localhost (css, images)

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
+import jinja2
 
 app = Flask(__name__)
+timer='12345'
+@app.route('/')
+def index():
+    return render_template('index.html', timer=timer)
 
 if __name__ == "__main__":
     app.run(debug=True)
