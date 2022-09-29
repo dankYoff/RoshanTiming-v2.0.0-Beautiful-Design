aba2fce291df9b22ac98631531eada3214dc7918
dankYoff <vsnopik@gmail.com>
2022-09-29 05:48:01 +0300

    Add TIMER

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from flask import Flask, render_template
-import jinja2
+from random import randint
 
 app = Flask(__name__)
-timer='12345'
+
+# RANDOM TIMER IN GAME
+a = randint(10, 50)
+if a <= 9:
+    aa = str('0' + str(a))
+else:
+    aa = str(str(a))
+b = randint(0, 59)
+if b <= 9:
+    bb = str('0' + str(b))
+else:
+    bb = str(str(b))
+timer = str(aa + ':' + bb)
+
 @app.route('/')
 def index():
     return render_template('index.html', timer=timer)
