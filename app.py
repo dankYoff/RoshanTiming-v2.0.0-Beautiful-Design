446d7e54325879db5d8e0a08e93e627b3cf61f70
dankYoff <vsnopik@gmail.com>
2022-10-03 03:33:59 +0300

    New version
    + JS, no python (ALL FUNCTIONAL)
    + Check correct and color input
    + Dont show link left in Github image

--- app.py
+++ app.py
@@ -3,47 +3,9 @@ from random import randint
 
 app = Flask(__name__)
 
-# RANDOM TIMER IN GAME
-a = randint(10, 50)
-if a <= 9:
-    aa = str('0' + str(a))
-else:
-    aa = str(str(a))
-b = randint(0, 59)
-if b <= 9:
-    bb = str('0' + str(b))
-else:
-    bb = str(str(b))
-timer = str(aa + ':' + bb)
-
-# AEGIS TIMER
-aegis_a = a + 5
-if aegis_a <= 9:
-    aegis_a = str('0' + str(aegis_a))
-else:
-    aegis_a = str(aegis_a)
-b = randint(0, 59)
-
-# MIN TIMER ROSHAN
-int_min_rosh = a + 8
-
-# MAX TIMER ROSHAN
-int_max_rosh = a + 11
-
-# Enter Aegis, min or max + Check
-c = 1
-d = str(str(aegis_a) + bb + ' ' + str(int_min_rosh) + bb + '-' + str(int_max_rosh) + bb)
-
-# Day/Night
-c = a // 5
-if c % 2 == 0:
-    k = "Ночь"
-else:
-    k = "День"
-
 @app.route('/')
 def index():
-    return render_template('index.html', timer=timer, d=d)
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
